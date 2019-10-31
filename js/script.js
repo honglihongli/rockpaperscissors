574d5ffb3923c331172a471e1ea4f1b38c6077d9
honglihongli <s_hong.li@ousd.org>
2019-10-31 01:20:20 +0000

    juice

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 // *************************** YOUR CODE BELOW *******************************
+$("document").ready(function() {
+$("#shoot").click(function(){
+    var input = $("#input").val();
+    $("#userChoice").text(input);
+    var computerchoice = Math.floor(Math.random()*3)
+    var array = ["Rock","Paper","Scissors"];
+    var computer = array[computerchoice];
+    $("#computerChoice").text(computer);
+})
+});
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
