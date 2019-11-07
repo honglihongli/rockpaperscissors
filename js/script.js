5ace8dafa10642e138f9492e8738acb29a1ad1ec
honglihongli <s_hong.li@ousd.org>
2019-11-07 02:08:19 +0000

    what

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
 // *************************** YOUR CODE BELOW *******************************
 $("document").ready(function() {
 $("#shoot").click(function(){
-    var input = $("#input").val();
-    $("#userChoice").text(input);
-    var computerchoice = Math.floor(Math.random()*3)
-    var array = ["Rock","Paper","Scissors"];
-    var computer = array[computerchoice];
-    $("#computerChoice").text(computer);
+  var input = $("#input").val();
+  $("#userChoice").text(input);
+  var computerchoice = Math.floor(Math.random()*3)
+  var array = ["Rock","Paper","Scissors"];
+  var computer = array[computerchoice];
+  $("#computerChoice").text(computer);
+  if (input === "Scissors" && computer === "Paper") {
+    $("#result").text("Player wins!")
+  } else if (input === "Scissors" && computer === "Rock") {
+    $("#result").text("Computer wins!");
+  } else if (input === "Scissors" && computer === "Scissors") {
+    $("#result").text("Nobody wins!");
+  } else if (input === "Paper" && computer === "Paper") {
+    $("#result").text("Nobody wins!")
+  } else if (input === "Paper" && computer === "Rock") {
+    $("#result").text("Player wins!");
+  } else if (input === "Paper" && computer === "Scissors") {
+    $("#result").text("Computer wins!");
+  } else if (input === "Rock" && computer === "Paper") {
+    $("#result").text("Computer wins!")
+  } else if (input === "Rock" && computer === "Rock") {
+    $("#result").text("Nobody wins!");
+  } else if (input === "Rock" && computer === "Scissors") {
+    $("#result").text("Player wins!");
+  }
 })
 });
 //******************TEST EARLY AND OFTEN USING console.log() ******************
